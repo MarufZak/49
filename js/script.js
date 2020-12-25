55c7b3fad8837541b6f9e77d0869547395fba881
Maruf <maruf@maruf.maruf>
2020-12-25 09:23:26 +0500

    allDone

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$('img').css({
+    display: 'block',
+    opacity: 0
+})
+
+$('#img1').fadeTo('fast',1).delay(12000).fadeTo('fast',0)
+setTimeout(() => {
+    $('#img2').fadeTo('fast',1).delay(11000).fadeTo('fast',0)
+}, 500)
+setTimeout(() => {
+    $('#img3').fadeTo('fast',1) .delay(10000).fadeTo('fast',0)
+}, 1000)
+setTimeout(() => {
+    $('#img4').fadeTo('fast',1) .delay(9000).fadeTo('fast',0)
+}, 1500)
+setTimeout(() => {
+    $('#img5').fadeTo('fast',1).delay(8000).fadeTo('fast',0)
+}, 2000)
+setTimeout(() => {
+    $('#img6').fadeTo('fast',1).delay(7000).fadeTo('fast',0)
+}, 2500)
+setTimeout(() => {
+    $('#img7').fadeTo('fast',1).delay(6000).fadeTo('fast',0)
+}, 3000)
+setTimeout(() => {
+    $('#img8').fadeTo('fast',1).delay(5000).fadeTo('fast',0)
+}, 3500)
+setTimeout(() => {
+    $('#img9').fadeTo('fast',1).delay(4000).fadeTo('fast',0)
+}, 4000)
+setTimeout(() => {
+    $('#img10').fadeTo('fast',1).delay(3000).fadeTo('fast',0)
+}, 4500)
+setTimeout(() => {
+    $('#img11').fadeTo('fast',1).delay(2000).fadeTo('fast',0)
+}, 5000)
+setTimeout(() => {
+    $('#img12').fadeTo('fast',1).delay(1000).fadeTo('fast',0)
+}, 5500)
